8b4dd43ccae9601a56864a8a42ee9edd60707c0b
Syed-Farhan-Ali-RS <fa1882793@gmail.com>
2024-01-22 16:31:00 +0500

    New Elements added

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,8 @@ const { check, validationResult } = require('express-validator');
 const User = require('../../models/User');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 // @route   POST api/users
 // @desc    Register User
 // @access  Public
@@ -54,7 +56,23 @@ router.post(
 
       // Return jsonwebtoken
 
-      return res.send('User Registered');
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 3600000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.log(err.message);
       res.status(500).send('Server Error');
