935bc89e31f4cc6c53b39ccfcedef9cb1f39c800
Syed-Farhan-Ali-RS <fa1882793@gmail.com>
2024-01-23 15:33:11 +0500

    Login and Authentication Completed...

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
-modules.exports = function (req, res, next) {
+module.exports = function (req, res, next) {
   // Get Token from header
   const token = req.header('x-auth-token');
 
