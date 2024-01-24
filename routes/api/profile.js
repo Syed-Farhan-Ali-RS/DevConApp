81d55d3e374f8423ac82ce4e90ef4d5c60a528f8
Syed-Farhan-Ali-RS <fa1882793@gmail.com>
2024-01-24 16:49:04 +0500

    Profilejs Update post router

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const { check, validationResult } = require('express-validator');
+
 // @route   GET api/profile/me
 // @desc    Get current users Profile
 // @access  Private
@@ -23,5 +25,26 @@ router.get('/me', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+// @route   POST api/profile
+// @desc    Create or Update User Profile
+// @access  Private
+
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('status', 'Status is required').not().isEmpty(),
+      check('skills', 'Skills is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  }
+);
 
 module.exports = router;
